d74a9bc256b9fc59a79078a14209939c089c336a
Ed Chun & Michael Ohm <pair+e7chun.michaelohm@devbootcamp.com>
2014-05-02 17:28:31 -0700

    mysterious changes

--- view.rb
+++ view.rb
@@ -17,34 +17,37 @@ class Display
 
   def self.welcome(available_meals)
     puts "Welcome to Make a Meal! \nWhat would you like to make?"
-    meals.each_with_index do |available_meals, index|
-      puts "#{index + 1}  #{meal}"
+    available_meals.each_with_index do |available_meals, index|
+      puts "#{index + 1}  #{available_meals}"
     end
   end
 
-  def self.list_meal_information(meal, ingredients_list, total_calories)
+  def self.puts_updated_table(meal_array)
+    counter = 1
+    meal_array.each do |food|
+      puts counter.to_s.ljust(3) + "[ ] #{food[0].ljust(15)}: #{food[1]}"
+      counter +=1
+    end
+  end
+
+  def self.list_meal_information(meal, meal_array)
     puts meal.upcase
     puts "-"*25
     "Ingredients".rjust(8) + " "*15 + "Calories"
-    puts @updated_table
+    puts_updated_table(meal_array)
     puts "-"*25
-    puts "Calorie Count" + " "*7 + "#{total_calories}"
-  end
+    # puts "Calorie Count" + " "*7 + "#{total_calories}"
 
-  self.list_meal_information{}
-
-  def self.puts_updated_table(updated_table)
-    @updated_table = updated_table
-    # counter = 1
-    # ingredients_list.each do |ingredient, calories|
-    #   puts counter.to_s.ljust(3) + "[ ] #{ingredient.ljust(15)}: #{calories}"
-    #   counter += 1
     end
   end
 
+  # self.list_meal_information{}
+
+
+
 
   def self.choose_ingredients(meal)
-    puts "Choose the ingredients for your #{meal} or "F" to finish."
+    puts "Choose the ingredients for your #{meal} or F to finish."
   end
 
   def self.delete_ingredient(ingredient)
@@ -59,7 +62,5 @@ meal = "burger"
 ingredients = {"lettuce" => 10,  "bacon" => 200, "pickle"=> 10, "onion" =>30}
 total_calories = 200
 
-Display.list_meal_information(meal,ingredients,total_calories)
-Display.choose_ingredients(meal)
 
 
